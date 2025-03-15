cc70285a52b5319df5847fdb80a39c16d88355c3
Robert090903 <amparorobert2@gmail.com>
2025-03-15 22:16:14 +0800

    Create web.php

---
+++ laravel_lab/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    Route::get('/test', function () {
+        return response()->json(['message' => 'API is working!']);
+    });
+});
